f112ae8760de9e69b469a0aeb677653b3e423734
Nayeli <Nayeli.Ayala4@icloud.com>
2025-02-05 18:51:47 -0700

    Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Nayeli"; 
+            int age = 30;
+            double avg = 98.7;
+            decimal banckaccount = 1000.00m;
+            char initial = 'N';
+            bool isSingle = true ;
+
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {banckaccount}\nInitial: {initial}\nSingle: {isSingle} ");
         }
     }
 }
